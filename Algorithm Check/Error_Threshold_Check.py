12ef9e8094cffe54505845780ccb53ad110bdd50
Hussanulmaab <h.ulmaab431@gmail.com>
2024-04-20 13:21:13 +0500

    Again committing after verifying

--- Algorithm Check/Error_Threshold_Check.py
+++ Algorithm Check/Error_Threshold_Check.py
@@ -127,6 +127,9 @@ print("Number of Error Bits are " + str(Error_Bits) + " out of " + str(len(Alice
 Error_Percentage = (Error_Bits/int(len(Alice_Bits))) * 100
 print("Error Percentage is " + str(Error_Percentage))
 
+
+
+
 # New Approach
 print("----------------------------------------------------------------------------------------------------------")
 print("-----------------------------------------------ROUND 3----------------------------------------------------")
@@ -166,6 +169,68 @@ with open("../New PseudoRandom Approach/Winnow_Round_3/Alice_Winnow-3_Final.txt"
     Alice_Bits = Alice_Sifted_bits.read()
 
 with open("../New PseudoRandom Approach/Winnow_Round_3/Bob_Winnow-3_Final.txt", 'r') as Bob_Sifted_bits:
+    Bob_Bits = Bob_Sifted_bits.read()
+
+print("\n-------------------------------------------After WINNOW ENCRYPTION -----------------------------------------\n")
+
+print("Alice Bits are : " + Alice_Bits)
+print("Bob bits are   : " + Bob_Bits)
+check_Error_String = ""
+Error_Bits = 0
+
+for i in range(0, int(len(Alice_Bits))):
+    if Alice_Bits[i] == Bob_Bits[i]:
+        check_Error_String = check_Error_String + " "
+    elif Alice_Bits[i] != Bob_Bits[i]:
+        check_Error_String = check_Error_String + "x"
+        Error_Bits += 1
+
+print("Error Bits     : " + check_Error_String)
+print("Number of Error Bits are " + str(Error_Bits) + " out of " + str(len(Alice_Bits)))
+
+Error_Percentage = (Error_Bits/int(len(Alice_Bits))) * 100
+print("Error Percentage is " + str(Error_Percentage))
+
+
+# New Approach
+print("----------------------------------------------------------------------------------------------------------")
+print("-----------------------------------------------ROUND 4----------------------------------------------------")
+print("----------------------------------------------------------------------------------------------------------")
+
+with open("../New PseudoRandom Approach/Winnow_Round_4/Alice_Permuted_4.txt", 'r') as Alice_Sifted_bits:
+    Alice_Bits = Alice_Sifted_bits.read()
+
+with open("../New PseudoRandom Approach/Winnow_Round_4/Bob_Permuted_4.txt", 'r') as Bob_Sifted_bits:
+    Bob_Bits = Bob_Sifted_bits.read()
+
+# Before Winnow Encryption
+print("\n-------------------------------------------BEFORE WINNOW ENCRYPTION -----------------------------------------\n")
+
+print("Alice Bits are : " + Alice_Bits)
+print("Bob bits are   : " + Bob_Bits)
+check_Error_String = ""
+Error_Bits = 0
+
+for i in range(0, int(len(Alice_Bits))):
+    if Alice_Bits[i] == Bob_Bits[i]:
+        check_Error_String = check_Error_String + " "
+    elif Alice_Bits[i] != Bob_Bits[i]:
+        check_Error_String = check_Error_String + "x"
+        Error_Bits += 1
+
+print("Error Bits     : " + check_Error_String)
+print("Number of Error Bits are " + str(Error_Bits) + " out of " + str(len(Alice_Bits)))
+
+Error_Percentage = (Error_Bits/int(len(Alice_Bits))) * 100
+print("Error Percentage is " + str(Error_Percentage))
+
+
+# After Winnow Encryption
+
+with open("../New PseudoRandom Approach/Winnow_Round_4/Alice_Winnow-4_Final.txt", 'r') as Alice_Sifted_bits:
+    Alice_Bits = Alice_Sifted_bits.read()
+
+with open("../New PseudoRandom Approach/Winnow_Round_4/Bob_Winnow-4_Final.txt", 'r') as Bob_Sifted_bits:
     Bob_Bits = Bob_Sifted_bits.read()
 
 print("\n-------------------------------------------After WINNOW ENCRYPTION -----------------------------------------\n")
---
+++ New PseudoRandom Approach/Winnow_Round_4/Permute_4.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+# Read data from Alice and Bob files
+with open("../Winnow_Round_3/Alice_Winnow-3_Final.txt", "r") as alice_file:
+    alice_data = np.array([int(bit) for bit in alice_file.read().strip()])
+
+with open("../Winnow_Round_3/Bob_Winnow-3_Final.txt", "r") as bob_file:
+    bob_data = np.array([int(bit) for bit in bob_file.read().strip()])
+
+# Generate a permutation
+permutation = np.random.permutation(len(alice_data))
+
+# Apply permutation to Alice's data
+alice_permuted = alice_data[permutation]
+
+# Apply the same permutation to Bob's data
+bob_permuted = bob_data[permutation]
+
+# Save permuted data to new files
+with open("Alice_Permuted_4.txt", "w") as alice_permuted_file:
+    alice_permuted_file.write("".join(map(str, alice_permuted)))
+
+with open("Bob_Permuted_4.txt", "w") as bob_permuted_file:
+    bob_permuted_file.write("".join(map(str, bob_permuted)))
+
+print("Files Alice_Permuted and Bob_Permuted saved successfully.")
